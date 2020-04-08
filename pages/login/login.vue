09fa32c9dbbd425338b635151713368110d7c0c1
Wyne_Jie <37859627+sefx5ever@users.noreply.github.com>
2020-04-09 00:27:13 +0800

    Update login.vue

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -42,11 +42,11 @@
 			this.pixelCalculation();
 			
 			var firebaseConfig = {
-				apiKey: "AIzaSyA2P0T3COrV1DHStIEBLnJoIuY4LunGmNg",
-				authDomain: "fir-test-84116.firebaseapp.com",
-				databaseURL: "https://fir-test-84116.firebaseio.com",
-				projectId: "fir-test-84116",
-				storageBucket: "fir-test-84116.appspot.com"
+				apiKey: "<API KEY>",
+				authDomain: "<AUTH DOMAIN>",
+				databaseURL: "<DB URL>",
+				projectId: "<PROJECT ID>",
+				storageBucket: "STORAGE BUCKET"
 			};
 			
 			// firebase.initializeApp(firebaseConfig);
